30cd9a8f49487e91ab742cc5e6dbdd227a60f373
Aadarsh <aadarsh.tussar@gmail.com>
2026-09-13 23:38:08 +0530

    invaild command message

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,5 +8,8 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  string command;
+  getline(cin, command);
+
+  cout<<command<<": command not found";
 }
