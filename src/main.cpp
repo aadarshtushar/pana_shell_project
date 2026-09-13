9af8e40d8b301011a27914dee690bd1e76e2b12b
Aadarsh <aadarsh.tussar@gmail.com>
2026-09-13 23:48:38 +0530

    Implement a REPL fix-3

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,6 @@ int main() {
     std::string command;
     std::getline(std::cin, command);
 
-    std::cout<<command<<": command not found";
+    std::cout<<command<<": command not found"<<endl;
   }
 }
