bc805fc28986b54978a5a03a4c756f6432b7571b
Aadarsh <aadarsh.tussar@gmail.com>
2026-09-17 17:49:16 +0530

    Implement PATH refactor-2

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,56 @@
 #include <unordered_map>
 #include <filesystem>
 #include <vector>
+#include <sstream>
+#include <utility>
 
-std::unordered_map <std::string, int> builtins{
-  {"type", 1},
-  {"exit", 1},
-  {"echo", 1}
+std::unordered_map <std::string, std::function<void(std::string)>> builtins{
+  {"echo", echo},
+  {"type", type},
+  {"exit", exit}
 };
 
-void type(std::string argument, std::vector<std::string> envDirectories){
-  if(builtins[argument] == 1) std::cout<<argument<<" is a shell builtin"<<"\n";
+void exit(std:: string argument){
+  return;
+}
+
+std::vector<std::string> envDirectories = []() {
+  
+  std::string paths = std::getenv("PATH");
+
+  std::istringstream pathStream (paths);
+
+  std::vector<std::string> directories;
+
+  std::string dir;
+  while(std::getline(ss, dir, ':')){
+    directories.push_back(dir);
+  }
+
+  return directories;
+}
+
+void type(std::string argument){
+  if(builtins.find(argument) != builtins.end()) std::cout<<argument<<" is a shell builtin"<<"\n";
   else{
-    bool flag = false;
 
-    for(auto i: envDirectories){
-
+    for(std::string i: envDirectories){
       std::filesystem::directory_iterator itr (i);
-      for(auto j: itr){
+
+      for(std::filesystem::directory_entry j: i){
         if(j.path().stem() != argument) continue;
 
         std::filesystem::perms p = j.status().permissions();
 
         std::filesystem::perms check = std::filesystem::perms::owner_exec |
-        std::filesystem::perms::group_exec |
-        std::filesystem::perms:: others_exec;
-
+          std::filesystem::perms::group_exec |
+          std::filesystem::perms::others_exec;
+        
         if((p & check) != std::filesystem::perms::none){
           std::string filepath = j.path();
           std::cout<<argument<<" is "<<filepath<<"\n";
           return;
         }
-
       }
     }
 
@@ -49,20 +69,6 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // parsing PATH environment variable to keep note of directories to be used to run commands
-  std::string path = std::getenv("PATH");
-  std::vector<std::string> envDirectories;
-  std::string dir = "";
-  for(auto i: path){
-    if(i == ':'){
-      envDirectories.push_back(dir);
-      dir = "";
-    }else{
-      dir += i;
-    }
-  }
-  envDirectories.push_back(dir);
-
   // Components of command line
   std::string commandLine;
   std::string command;
@@ -72,27 +78,28 @@ int main() {
   while(true){
 
     std::cout<<"$ ";
-    commandLine = "";
     std::getline(std::cin, commandLine);
 
     // seperating command and argument from commandline
     int size = commandLine.length();
     int index = 0;
 
-    command = "";
     while(commandLine[index] != ' ' && index < size){
       command += commandLine[index++];
     }
 
     index++;
-    argument = "";
     while(index < size){
       argument += commandLine[index++];
     }
 
     if(command == "exit") break;
-    else if(command == "echo") echo(argument);
-    else if(command == "type") type(argument, envDirectories);
-    else std::cout<<command<<": command not found"<<"\n";
+
+    std::pair<std::string, std::function<void(std::string)>>::iterator it = builtins.find(command);
+    if(it != builtins.end()){
+      it->second(argument);
+    }else{
+      std::cout<<command<<": command not found"<<"\n";
+    }
   }
 }
