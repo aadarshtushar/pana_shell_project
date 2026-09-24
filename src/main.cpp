708693043220fc513cc099825629956974b624e5
Aadarsh <aadarsh.tussar@gmail.com>
2026-09-25 03:07:30 +0530

    Implement backslash inside single quotes

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ std::string stringParser(std::string argument){
 
     for(int i = 0; i < size; i++){
 
-      if(argument[i] == '\\'){
+      if(argument[i] == '\\' && !isMinorStringFlag){
         i++;
         if(i < size){
           if(argument[i] == ' '){
